891dfa9a6091d5c1e9f1d49394f1b2d703354e74
aidexx <yermakovich.nikita@gmail.com>
2021-07-21 17:31:52 +0300

    Added Decorator examples

---
+++ Homework-3/Decorator/PerformerDecorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3.Decorator
+{
+    public class PerformerDecorator : IPerformer
+    {
+        protected IPerformer Performer;
+
+        public PerformerDecorator(IPerformer component)
+        {
+            Performer = component;
+        }
+        public virtual void Perform()
+        {
+            Performer.Perform();
+        }
+
+    }
+}
--- Homework-3/Program.cs
+++ Homework-3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Homework_3.Decorator;
 using SolidCorrectClass;
 using SolidIncorrectClass;
 
@@ -10,6 +13,20 @@ namespace Homework_3
         {
             var p = new SolidIncorrectClass.Child() {Value1 = 1, Value2 = 2};
 
+
+            //Декоратор
+
+            var list = new List<IPerformer>
+            {
+                new MagicianDecorator(new Performer("Vasya")),
+                new DancerDecorator(new MagicianDecorator(new Performer("Dima")))
+            };
+
+            foreach (var i in list)
+            {
+                i.Perform();
+                Console.WriteLine(new string('_', 50));
+            }
         }
     }
 }
